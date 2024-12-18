2bc9d522f52779e00356f77a7728e91522d14d95
Gus7av0x <gustavovagner596@gmail.com>
2024-12-18 11:36:55 -0300

    atuaizacao2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reactvite/', // Caminho base ajustado para o repositório
+  base: '/reactvite/', // Substitua 'reactvite' pelo nome do seu repositório
 });

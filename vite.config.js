f9ab10db7e61fe406951c1013819dcd66af9b432
Gus7av0x <gustavovagner596@gmail.com>
2024-12-17 22:15:45 -0300

    Corrigindo base no vite.config.js para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
- 
+ base: '/reactvite/',  // Certifique-se de que está usando '/reactvite/'
   resolve: {
     alias: {
       '@services': '/src/services', // Alias para a pasta de services
